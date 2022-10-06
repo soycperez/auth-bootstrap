0c38578fb0cb03cbbc5fef823b75033c83024fa4
soycperez <soycesar20k18@gmail.com>
2022-10-06 16:04:20 -0500

    first commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth::routes();
+
+
+Auth::routes(['login' => false,'register' => false]);
+
+
+// // Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+// Route::post('login', 'Auth\LoginController@login');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
