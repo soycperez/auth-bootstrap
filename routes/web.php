3089204a91260e5ae517f60a93cfaf1dd19e59a4
soycperez <soycesar20k18@gmail.com>
2022-10-18 20:49:17 -0500

    Modificacion y personalización de rutas de bootstrap auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,19 +13,33 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('private')->group(function (){
+    Auth::routes();
+    Route::middleware(['auth', 'verified'])->group(function () {
+        Route::get('/', function () {
+            return view('welcome');
+        });
+    });
+    Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
 });
 
-// Auth::routes();
 
 
-Auth::routes(['login' => false,'register' => false]);
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+//Auth::routes();
 
 
-// // Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+//Auth::routes(['login' => false,'register' => false]);
+
+
+//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+
+//Route::get('/register',[App\Http\Controllers\Auth\LoginController::class], 'showRegistrationForm')->name('register');
 // Route::post('login', 'Auth\LoginController@login');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+//Route::middleware(['auth'])->
